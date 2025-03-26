16c31565b88f298d3a86fd1f415f1aae7b91bd66
JeonChangHyeon <jkp9989@gmail.com>
2025-03-26 16:50:26 +0900

    SmartLight2D Import

---
+++ Assets/02_Scripts/Test/Test_InventoryAdd.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Test_InventoryAdd : MonoBehaviour
+{
+    public UI_Inventory inventory;
+    public void AddItem(ItemSO item)
+    {
+        InventoryManager.Instance.AddItem(item);
+        inventory.UpdatUI();
+    }
+}
